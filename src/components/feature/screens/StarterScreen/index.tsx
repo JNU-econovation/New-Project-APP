95ba194f2041e27e2aa8733d8e5fc29dac17f12e
박건규 <geongyu09@gmail.com>
2025-04-03 21:42:45 +0900

    feat: 임의의 starter screen 추가
    아직 디자인이 정해지지 않아 임의로 만든 starter screen 컴포넌트 구현

---
+++ src/components/feature/screens/StarterScreen/index.tsx
@@ -0,0 +1,33 @@
+import PositionBottom from "@components/common/shared/layout/PositionBottom";
+import Button from "@components/common/shared/ui/Button";
+import styled from "@emotion/native";
+
+const StarterScreen = () => {
+  return (
+    <Container>
+      <StyledText>Hello, this is the starter screen!</StyledText>
+      <StyledImage source={require("@/assets/images/react-logo.png")} />
+      <PositionBottom>
+        <Button title="로그인 하러 가기" fullWidth />
+      </PositionBottom>
+    </Container>
+  );
+};
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  align-items: center;
+`;
+
+const StyledText = styled.Text`
+  font-size: 20px;
+  color: #333;
+`;
+
+const StyledImage = styled.Image`
+  width: 100px;
+  height: 100px;
+  margin-top: 20px;
+`;
+
+export default StarterScreen;
