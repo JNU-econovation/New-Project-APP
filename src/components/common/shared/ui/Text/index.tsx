bd9052007e7e7aa60409862ce9e99d89bbd12930
박건규 <geongyu09@gmail.com>
2025-04-06 21:43:03 +0900

    feat: Text 공통 컴포넌트의 기본 색상을 검정생으로 변경
    - colorPalette에 black 색상 추가
    - Text 컴포넌트의 기본 색상을 black으로 변경

--- src/components/common/shared/ui/Text/index.tsx
+++ src/components/common/shared/ui/Text/index.tsx
@@ -1,16 +1,18 @@
-import styled, { css } from "@emotion/native";
 import { COLORS } from "@/src/styles/colorPalette";
+import styled from "@emotion/native";
 
 interface TextProps {
   fontSize?: number;
   color?: keyof typeof COLORS;
   fontWeight?: string;
   textAlign?: string;
+  opacity?: number;
 }
 
 export default styled.Text<TextProps>`
   font-size: ${({ fontSize = 16 }) => `${fontSize}px`};
-  color: ${({ color = "mainGreen" }) => COLORS[color]};
+  color: ${({ color = "black" }) => COLORS[color]};
   font-weight: ${({ fontWeight = "normal" }) => fontWeight};
   text-align: ${({ textAlign = "left" }) => textAlign};
+  opacity: ${({ opacity = 1 }) => opacity};
 `;
