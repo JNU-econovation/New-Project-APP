79ff2cdad13e9425fe9881cfd7e4e3e979049187
박건규 <geongyu09@gmail.com>
2025-04-07 00:22:12 +0900

    feat: 로그인 페이지로 리다이렉트 될 수 있도록 기능 구현
    로그인 페이지를 stack 에 넣어두고, 로그인 버튼을 눌렀을 때 route 되도록 기능 구현

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,15 +2,14 @@ import StarterScreen from "@components/feature/screens/Starter/StarterScreen";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  const isAuthenticated = false;
-
-  if (!isAuthenticated) {
-    return <StarterScreen />;
-  }
-
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{}} />
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen name="login" />
     </Stack>
   );
 }
