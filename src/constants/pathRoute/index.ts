630484e11082da2444f226ec47bcdcb52705e7c5
박건규 <geongyu09@gmail.com>
2025-04-07 00:19:56 +0900

    feat: Path Route 관련 상수 데이터 추가

---
+++ src/constants/pathRoute/index.ts
@@ -0,0 +1,13 @@
+const webviewBaseUri = process.env.EXPO_PUBLIC_WEB_BASE_URI;
+
+const WEBVIEW = {
+  BASE_URL: webviewBaseUri,
+  LOGIN: `${webviewBaseUri}/login`,
+  EMAIL_AUTH: `${webviewBaseUri}/email-auth`,
+} as const;
+
+const PATH_ROUTE = {
+  WEBVIEW,
+} as const;
+
+export default PATH_ROUTE;
