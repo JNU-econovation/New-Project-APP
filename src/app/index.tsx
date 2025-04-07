3ccbf9b2ca08bcf6a9eb3e12a0cfb82b732250e7
박건규 <geongyu09@gmail.com>
2025-04-07 09:28:25 +0900

    feat: splash 화면 구현
    splash 화면을 구현합니다.
    여기서 개발자 등록이 안되어 빌드가 안되는 상황이라 올바르게 구현이 되었는지 확인이 어려운 상태입니다.
    결제 후 테스트를 할 예정이며, 관련해서 문제가 존재한다면 추가 pr 작성하도록 하겠습니다
    
    #1

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,42 @@
 import LoginButton from "@components/feature/screens/Starter/LoginButton";
 import StarterScreen from "@components/feature/screens/Starter/StarterScreen";
+import { SplashScreen } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
 import { Text, View } from "react-native";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// SplashScreen.setOptions({
+//   duration: 1000,
+//   fade: true,
+// });
 
 export default function Index() {
   const isAuthenticated = false;
+
+  const [appIsReady, setAppIsReady] = useState(false);
+  console.log("appIsReady", appIsReady);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
 
   if (!isAuthenticated) {
     return <StarterScreen />;
