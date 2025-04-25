c5d2bc3e01bac2f7270fb6db9a40b3cb324c38e2
박건규 <geongyu09@gmail.com>
2025-04-25 15:01:25 +0900

    feat: svg 파일을 사용하도록 코드 수정
    svg 파일을 사용하도록 하였습니다.
    이때 모든 svg파일은 icon.ts 에서 re-export 하여 사용합니다.
    
    #2

---
+++ src/components/common/shared/ui/Icons/index.tsx
@@ -0,0 +1,9 @@
+// Arrow
+export { default as LeftArrowSVG } from "@assets/images/icons/Left_Arrow.svg";
+export { default as RightArrowSVG } from "@assets/images/icons/Right_Arrow.svg";
+export { default as LeftArrowBlackSVG } from "@assets/images/icons/Left_Arrow_Black.svg";
+
+// [Screen] Starter
+export { default as Guide1SVG } from "@assets/images/screens/starter/Guide1.svg";
+export { default as Guide2SVG } from "@assets/images/screens/starter/Guide2.svg";
+export { default as Guide3SVG } from "@assets/images/screens/starter/Guide3.svg";
