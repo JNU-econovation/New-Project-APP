8fbe471a7a3d9db4e19dcf505b0519b3689e8700
박건규 <geongyu09@gmail.com>
2025-04-26 22:35:57 +0900

    feat: 카카오 로그인 버튼 UI 구현

--- src/components/common/shared/ui/Icons/index.tsx
+++ src/components/common/shared/ui/Icons/index.tsx
@@ -4,3 +4,4 @@ export { default as RightArrowSVG } from "@assets/images/icons/Right_Arrow.svg";
 export { default as LeftArrowBlackSVG } from "@assets/images/icons/Left_Arrow_Black.svg";
 export { default as StarSVG } from "@assets/images/icons/Star.svg";
 export { default as HikingSVG } from "@assets/images/icons/Hiking.svg";
+export { default as KakaoSVG } from "@assets/images/icons/Kakao.svg";
