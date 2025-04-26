4715c4548e4b1d675c4243b7d048de02fa511b3a
박건규 <geongyu09@gmail.com>
2025-04-26 21:25:28 +0900

    feat: 로그인 페이지에 필요한 svg 파일을 사용할 수 있도록 icons/index.ts에 추가
    #5

--- src/components/common/shared/ui/Icons/index.tsx
+++ src/components/common/shared/ui/Icons/index.tsx
@@ -2,3 +2,5 @@
 export { default as LeftArrowSVG } from "@assets/images/icons/Left_Arrow.svg";
 export { default as RightArrowSVG } from "@assets/images/icons/Right_Arrow.svg";
 export { default as LeftArrowBlackSVG } from "@assets/images/icons/Left_Arrow_Black.svg";
+export { default as StarSVG } from "@assets/images/icons/Star.svg";
+export { default as HikingSVG } from "@assets/images/icons/Hiking.svg";
