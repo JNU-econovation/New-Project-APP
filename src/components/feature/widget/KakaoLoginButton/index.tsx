56779b396559e897a205d215fbf60dc3727994b9
박건규 <geongyu09@gmail.com>
2025-04-26 22:35:57 +0900

    feat: 카카오 로그인 버튼 UI 구현

---
+++ src/components/feature/widget/KakaoLoginButton/index.tsx
@@ -0,0 +1,24 @@
+import Button from "@components/common/shared/ui/Button";
+import { KakaoSVG } from "@components/common/shared/ui/Icons";
+import styled from "@emotion/native";
+
+const KakaoLoginButton = () => {
+  return (
+    <Container>
+      <Button
+        title="카카오톡으로 3초만에 시작하기"
+        backgroundColor="kakaoYellow"
+        color="black"
+        fullWidth
+        startIcon={<KakaoSVG />}
+      />
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  flex: 1;
+  width: 100%;
+`;
+
+export default KakaoLoginButton;
