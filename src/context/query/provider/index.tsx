6ec0662309635835d59e54491b815b071233aafb
박건규 <geongyu09@gmail.com>
2025-04-28 17:03:29 +0900

    feat: tanstack query devtool 추가
    #5

--- src/context/query/provider/index.tsx
+++ src/context/query/provider/index.tsx
@@ -1,9 +1,22 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "../client";
+import * as Clipboard from "expo-clipboard";
 import { PropsWithChildren } from "react";
+import { DevToolsBubble } from "react-native-react-query-devtools";
+import queryClient from "../client";
 
 export default function QueryProvider({ children }: PropsWithChildren) {
+  const onCopy = async (text: string) => {
+    try {
+      await Clipboard.setStringAsync(text);
+      return true;
+    } catch {
+      return false;
+    }
+  };
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <DevToolsBubble onCopy={onCopy} />
+    </QueryClientProvider>
   );
 }
