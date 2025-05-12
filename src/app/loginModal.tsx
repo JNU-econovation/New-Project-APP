4930fde72779368c4f92001a2d98403f96b0d6d5
박건규 <geongyu09@gmail.com>
2025-05-12 10:13:37 +0900

    feat: 로그인 웹 뷰 화면의 토큰 저장 로직 추가
    #5

--- src/app/loginModal.tsx
+++ src/app/loginModal.tsx
@@ -1,30 +1,10 @@
-import WebViewWithInjected from "@components/common/entities/WebViewWithInjected";
-import PATH_ROUTE from "@constants/pathRoute";
+import LoginWebView from "@components/feature/widget/LoginWebView";
 import styled from "@emotion/native";
-import { useCallback, useRef } from "react";
-import WebView from "react-native-webview";
-import { requestKakaoBridge } from "@bridges/request-kakao";
-import { postMessage } from "@utils/bridge";
 
 const LoginModalScreen = () => {
-  const ref = useRef<WebView>(null);
-
-  const requestKakaoLoginBridge = useCallback(() => {
-    postMessage(ref, requestKakaoBridge());
-  }, []);
-
   return (
     <Container>
-      <WebViewWithInjected
-        ref={ref}
-        source={{ uri: PATH_ROUTE.WEBVIEW.LOGIN }}
-        onReadyToMessage={requestKakaoLoginBridge}
-        onMessage={(reqMessage) => {
-          if (reqMessage.name === "request-kakao") {
-            console.log("request-kakao", reqMessage);
-          }
-        }}
-      />
+      <LoginWebView />
     </Container>
   );
 };
