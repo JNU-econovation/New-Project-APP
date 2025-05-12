dc51b85e1b433bf83db59b10e0b87697ed6426f6
박건규 <geongyu09@gmail.com>
2025-05-12 22:01:35 +0900

    feat: 탭바에 들어가는 아이콘 정적데이터를 사용할 수 있도록 재수출문 추가
    #11

--- src/components/common/shared/ui/Icons/index.tsx
+++ src/components/common/shared/ui/Icons/index.tsx
@@ -6,3 +6,14 @@ export { default as StarSVG } from "@assets/images/icons/Star.svg";
 export { default as HikingSVG } from "@assets/images/icons/Hiking.svg";
 export { default as KakaoSVG } from "@assets/images/icons/Kakao.svg";
 export { default as AppleSVG } from "@assets/images/icons/Apple.svg";
+
+// Tab_Block
+export { default as HomeBlockSVG } from "@assets/images/icons/Home-Block.svg";
+export { default as MapBlockSVG } from "@assets/images/icons/Map-Block.svg";
+
+// Tab_Week
+export { default as HomeSVG } from "@assets/images/icons/Home.svg";
+export { default as MountainSVG } from "@assets/images/icons/Mountain.svg";
+export { default as MapSVG } from "@assets/images/icons/Map.svg";
+export { default as StoreSVG } from "@assets/images/icons/Store.svg";
+export { default as HeroSVG } from "@assets/images/icons/Hero.svg";
