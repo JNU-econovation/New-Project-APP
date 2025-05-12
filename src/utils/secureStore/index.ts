e316d9c7df32152197257f4b68cd0941d76aa7f3
박건규 <geongyu09@gmail.com>
2025-05-12 10:13:00 +0900

    feat: SecureStore를 사용할 수 있는 유틸 함수 추가
    #5

---
+++ src/utils/secureStore/index.ts
@@ -0,0 +1,7 @@
+import * as SecureStore from "expo-secure-store";
+
+export const getValueFromSecureStore = async (key: string) =>
+  (await SecureStore.getItemAsync(key)) || null;
+
+export const setValueToSecureStore = async (key: string, value: string) =>
+  await SecureStore.setItemAsync(key, value);
