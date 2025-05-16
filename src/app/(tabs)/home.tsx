e40a68ce3b1ce24c0a66314128cc241bfced3273
박건규 <geongyu09@gmail.com>
2025-05-16 15:36:14 +0900

    feat: 홈 화면 구현

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,12 +1,32 @@
-import Text from "@components/common/shared/ui/Text";
-import { View } from "react-native";
+import Spacing from "@components/common/shared/layout/Spacing";
+import HomeCanvas from "@components/feature/screens/Home/HomeCanvas";
+import HomeHeaderSection from "@components/feature/screens/Home/HomeHeaderSection";
+import HomeNavGridSection from "@components/feature/screens/Home/HomeNavGridSection";
+import styled from "@emotion/native";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>hi</Text>
-    </View>
+    <Screen>
+      <Container>
+        <Spacing gap={20} />
+        <HomeHeaderSection />
+        <Spacing gap={20} />
+        <HomeCanvas />
+        <HomeNavGridSection />
+        <Spacing gap={20} />
+      </Container>
+    </Screen>
   );
 };
+
+const Screen = styled.SafeAreaView`
+  flex: 1;
+  background-color: #efefef;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  padding-inline: 20px;
+`;
 
 export default HomeScreen;
