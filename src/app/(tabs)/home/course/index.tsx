991523e7b71b7ff8f4513d12eba0f1760b709eb0
박건규 <geongyu09@gmail.com>
2025-05-20 23:56:43 +0900

    refactor: 산 검색 페이지에서 로딩바가 보이도록 수정
    #15

--- src/app/(tabs)/home/course/index.tsx
+++ src/app/(tabs)/home/course/index.tsx
@@ -1,7 +1,12 @@
 import MountainSearchWebview from "@components/feature/screens/course/MountainSearchWebview";
+import { SafeAreaView } from "react-native";
 
 const CourseScreen = () => {
-  return <MountainSearchWebview />;
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <MountainSearchWebview />
+    </SafeAreaView>
+  );
 };
 
 export default CourseScreen;
