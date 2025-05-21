3495811d07aa59ac0a7052137ecd98b970c5cbc1
박건규 <geongyu09@gmail.com>
2025-05-21 20:10:54 +0900

    feat: 코스 부분 웹뷰 section 구현
    #15

--- src/app/(tabs)/home/course/[mountainName].tsx
+++ src/app/(tabs)/home/course/[mountainName].tsx
@@ -1,23 +1,10 @@
-import WebViewWithInjected from "@components/common/entities/WebViewWithInjected";
-import PATH_ROUTE from "@constants/pathRoute";
-import { router, useLocalSearchParams } from "expo-router";
+import MountainCourseWebview from "@components/feature/screens/course/MountainCourseWebview";
 import { SafeAreaView } from "react-native";
 
 const MountainCourseScreen = () => {
-  const { mountainName } = useLocalSearchParams() as {
-    mountainName: string;
-  };
-
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
-      <WebViewWithInjected
-        source={{
-          uri: PATH_ROUTE.WEBVIEW.COURSE_SEARCH_RESULT({ mountainName }),
-        }}
-        onMessage={({ method, name }) => {
-          if (name === "route-back" && method === "POST") router.back();
-        }}
-      />
+      <MountainCourseWebview />
     </SafeAreaView>
   );
 };
