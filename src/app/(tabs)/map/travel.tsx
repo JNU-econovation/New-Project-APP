ed274a9dfb1907a35f8dfcb89771c449356f8fc2
박건규 <geongyu09@gmail.com>
2025-05-26 10:41:33 +0900

    feat: 산행 페이지 추가
    #24

---
+++ src/app/(tabs)/map/travel.tsx
@@ -0,0 +1,12 @@
+import Text from "@components/common/shared/ui/Text";
+import { View } from "react-native";
+
+const TravelScreen = () => {
+  return (
+    <View>
+      <Text>TravelScreen</Text>
+    </View>
+  );
+};
+
+export default TravelScreen;
