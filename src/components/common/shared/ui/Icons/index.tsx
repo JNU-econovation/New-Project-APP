70a37b1b51939ad0097ec1f3a14f276451178483
박건규 <geongyu09@gmail.com>
2025-05-28 16:11:28 +0900

    chore: icons 정적 데이터 위치를 변경함에 따라 icons import 문 수정
    #20

--- src/components/common/shared/ui/Icons/index.tsx
+++ src/components/common/shared/ui/Icons/index.tsx
@@ -1,23 +1,26 @@
 // Arrow
-export { default as LeftArrowSVG } from "@assets/images/icons/Left_Arrow.svg";
-export { default as RightArrowSVG } from "@assets/images/icons/Right_Arrow.svg";
-export { default as LeftArrowBlackSVG } from "@assets/images/icons/Left_Arrow_Black.svg";
-export { default as StarSVG } from "@assets/images/icons/Star.svg";
-export { default as HikingSVG } from "@assets/images/icons/Hiking.svg";
-export { default as KakaoSVG } from "@assets/images/icons/Kakao.svg";
-export { default as AppleSVG } from "@assets/images/icons/Apple.svg";
+export { default as LeftArrowSVG } from "@assets/icons/Left_Arrow.svg";
+export { default as RightArrowSVG } from "@assets/icons/Right_Arrow.svg";
+export { default as LeftArrowWithWhiteBgSVG } from "@assets/icons/Left_Arrow_Black.svg";
+export { default as BackArrow } from "@assets/icons/Back_Arrow.svg";
+
+//image
+export { default as StarSVG } from "@assets/icons/Star.svg";
+export { default as HikingSVG } from "@assets/icons/Hiking.svg";
+export { default as KakaoSVG } from "@assets/icons/Kakao.svg";
+export { default as AppleSVG } from "@assets/icons/Apple.svg";
 
 // Tab_Block
-export { default as HomeBlockSVG } from "@assets/images/icons/Home-Block.svg";
-export { default as MapBlockSVG } from "@assets/images/icons/Map-Block.svg";
+export { default as HomeBlockSVG } from "@assets/icons/Home_Block.svg";
+export { default as MapBlockSVG } from "@assets/icons/Map_Block.svg";
 
 // Tab_Week
-export { default as HomeSVG } from "@assets/images/icons/Home.svg";
-export { default as MountainSVG } from "@assets/images/icons/Mountain.svg";
-export { default as MapSVG } from "@assets/images/icons/Map.svg";
-export { default as StoreSVG } from "@assets/images/icons/Store.svg";
-export { default as HeroSVG } from "@assets/images/icons/Hero.svg";
+export { default as HomeSVG } from "@assets/icons/Home.svg";
+export { default as MountainSVG } from "@assets/icons/Mountain.svg";
+export { default as MapSVG } from "@assets/icons/Map.svg";
+export { default as StoreSVG } from "@assets/icons/Store.svg";
+export { default as HeroSVG } from "@assets/icons/Hero.svg";
 
 // Header
-export { default as BellSVG } from "@assets/images/icons/Bell.svg";
-export { default as SettingSVG } from "@assets/images/icons/Setting.svg";
+export { default as BellSVG } from "@assets/icons/Bell.svg";
+export { default as SettingSVG } from "@assets/icons/Setting.svg";
