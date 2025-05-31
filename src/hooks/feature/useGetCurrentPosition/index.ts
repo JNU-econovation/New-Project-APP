254150acb95bcf6c1dd5dc8ee99e97f223b07a55
박건규 <geongyu09@gmail.com>
2025-05-31 21:41:29 +0900

    feat: useGetCurrentPosition 훅이 비동기 동작이 포함됨에 따라서 isLoading 상태 추가
    #20

--- src/hooks/feature/useGetCurrentPosition/index.ts
+++ src/hooks/feature/useGetCurrentPosition/index.ts
@@ -5,11 +5,13 @@ const useGetCurrentPosition = () => {
   const [location, setLocation] = useState<Location.LocationObject | null>(
     null,
   );
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
   useEffect(() => {
     async function getCurrentLocation() {
       let { status } = await Location.requestForegroundPermissionsAsync();
+      setIsLoading(false);
       if (status !== "granted") {
         setErrorMsg("위치 서비스 접근 권한이 필요합니다.");
         return;
@@ -24,6 +26,7 @@ const useGetCurrentPosition = () => {
 
   return {
     location,
+    isLoading,
     errorMsg,
   };
 };
