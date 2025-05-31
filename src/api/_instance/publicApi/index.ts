93b84712e6d6348a78112a4a0396175840d86e7e
박건규 <geongyu09@gmail.com>
2025-05-31 15:29:36 +0900

    chore: axios timeout이 너무 짧아 올바른 응답을 기대할 수 없어 5초로 변경

--- src/api/_instance/publicApi/index.ts
+++ src/api/_instance/publicApi/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const publicApi = axios.create({
   baseURL: process.env.BASE_URI,
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
     Accept: "*/*",
