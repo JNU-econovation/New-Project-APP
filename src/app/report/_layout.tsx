6ed2d7653e13a374de9166eddc04264b0160cab8
박건규 <geongyu09@gmail.com>
2025-05-31 21:43:22 +0900

    feat: reort _layout 구현
    #20

---
+++ src/app/report/_layout.tsx
@@ -0,0 +1,28 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <StatusBar barStyle={"dark-content"} />
+      <Stack.Screen
+        name="index"
+        options={{
+          animation: "fade",
+          animationDuration: 100,
+        }}
+      />
+      <Stack.Screen
+        name="immediatelyReport"
+        options={{
+          animation: "fade",
+          animationDuration: 100,
+        }}
+      />
+    </Stack>
+  );
+}
