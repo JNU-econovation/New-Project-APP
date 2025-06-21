756d652afc94334b1143547c8094c13115aab7a6
박건규 <geongyu09@gmail.com>
2025-06-21 16:40:25 +0900

    feat: Textarea 공통 ui 컴포넌트 구현
    #20

---
+++ src/components/common/shared/ui/Textarea/index.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/native";
+import { COLORS } from "@styles/colorPalette";
+
+const Textarea = styled.TextInput`
+  border: 1px solid ${COLORS.gray200};
+  border-radius: 8px;
+  padding: 12px;
+`;
+
+export default Textarea;
