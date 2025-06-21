b816518cccd5acb5fa3055aad8c55b924334dc5d
박건규 <geongyu09@gmail.com>
2025-06-21 16:39:41 +0900

    chore: gray-200 색 추가
    #20

--- src/styles/colorPalette.ts
+++ src/styles/colorPalette.ts
@@ -2,6 +2,7 @@ export const COLORS = {
   mainGreen: "#2b7552",
   mainWhite: "#ffffff",
   subGray: "#dadada",
+  gray200: "#828282",
   black: "#000000",
   kakaoYellow: "#FAE64D",
   mainRed: "#FF5151",
