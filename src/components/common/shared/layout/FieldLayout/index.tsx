55b76d7a129a97443be192aecd17afe88a3106a0
박건규 <geongyu09@gmail.com>
2025-06-22 15:59:58 +0900

    feat: 반복되는 form의 field layout을 동통 컴포넌트로 정의
    #20

---
+++ src/components/common/shared/layout/FieldLayout/index.tsx
@@ -0,0 +1,36 @@
+import styled from "@emotion/native";
+import Spacing from "@shared/layout/Spacing";
+import Text from "@shared/ui/Text";
+
+interface FieldLayoutProps {
+  title: string;
+  titleSideComponent?: React.ReactNode;
+  content: React.ReactNode;
+}
+
+const FieldLayout = ({
+  title,
+  titleSideComponent,
+  content,
+}: FieldLayoutProps) => {
+  return (
+    <>
+      <HeaderContainer style={{ flexDirection: "row", alignItems: "center" }}>
+        <Text fontWeight="bold" fontSize={20}>
+          {title}
+        </Text>
+        {titleSideComponent && titleSideComponent}
+      </HeaderContainer>
+      <Spacing size={14} />
+      {content}
+    </>
+  );
+};
+
+const HeaderContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+`;
+
+export default FieldLayout;
