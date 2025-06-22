8c68660f4c508408bef65d7fe79161fd60696728
박건규 <geongyu09@gmail.com>
2025-06-22 16:55:50 +0900

    feat: Textarea 공통 ui 컴포넌트에 backgroundColor를 사용할 수 있도록 추가
    #20

--- src/components/common/shared/ui/Textarea/index.tsx
+++ src/components/common/shared/ui/Textarea/index.tsx
@@ -1,10 +1,16 @@
 import styled from "@emotion/native";
 import { COLORS } from "@styles/colorPalette";
 
-const Textarea = styled.TextInput`
+interface StyledTextInputProps {
+  backgroundColor?: keyof typeof COLORS;
+}
+
+const Textarea = styled.TextInput<StyledTextInputProps>`
   border: 1px solid ${COLORS.gray200};
   border-radius: 8px;
   padding: 12px;
+  background-color: ${({ backgroundColor }) =>
+    backgroundColor ? COLORS[backgroundColor] : COLORS.mainWhite};
 `;
 
 export default Textarea;
