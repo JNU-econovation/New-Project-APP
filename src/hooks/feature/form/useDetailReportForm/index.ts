0fdf04a25a5b83abc3e5a8294e4e875fc9bff3c6
박건규 <geongyu09@gmail.com>
2025-06-22 16:57:59 +0900

    feat: 상세 신고 페이지에서 사용되는 훅을 위한 form 훅 구현
    #20

---
+++ src/hooks/feature/form/useDetailReportForm/index.ts
@@ -0,0 +1,25 @@
+import { useAppForm } from "@service/form/hooks";
+import { formOptions } from "@tanstack/react-form";
+
+export const detailReportForm = formOptions({
+  defaultValues: {
+    content: "",
+    attachments: [],
+    address: "",
+  },
+});
+
+const useDetailReportForm = () =>
+  useAppForm({
+    ...detailReportForm,
+    validators: {
+      onChange: ({ value }) => {
+        console.log("onChange value:", value);
+      },
+    },
+    onSubmit: ({ value }) => {
+      alert(JSON.stringify(value, null, 2));
+    },
+  });
+
+export default useDetailReportForm;
