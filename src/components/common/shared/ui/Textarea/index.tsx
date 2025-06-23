7c145c1656de681ac629ea6ea484a049c3afeb3e
박건규 <geongyu09@gmail.com>
2025-06-23 16:31:16 +0900

    feat: Textarea 공통 ui 컴포넌트에 font size 와 color를 입력받을 수 있도록 수정
    #20

--- src/components/common/shared/ui/Textarea/index.tsx
+++ src/components/common/shared/ui/Textarea/index.tsx
@@ -3,14 +3,20 @@ import { COLORS } from "@styles/colorPalette";
 
 interface StyledTextInputProps {
   backgroundColor?: keyof typeof COLORS;
+  width?: string | number;
+  height?: string | number;
+  fontSize?: number;
+  color?: keyof typeof COLORS;
 }
 
 const Textarea = styled.TextInput<StyledTextInputProps>`
-  border: 1px solid ${COLORS.gray200};
+  border: 1px solid ${COLORS.subGray};
   border-radius: 8px;
   padding: 12px;
   background-color: ${({ backgroundColor }) =>
     backgroundColor ? COLORS[backgroundColor] : COLORS.mainWhite};
+  font-size: ${({ fontSize }) => (fontSize ? `${fontSize}px` : "16px")};
+  color: ${({ color }) => (color ? COLORS[color] : COLORS.black)};
 `;
 
 export default Textarea;
