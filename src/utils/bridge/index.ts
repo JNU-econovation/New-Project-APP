9755bfc9a67c948c3f3f7bfbbd3e9eedeaba8db2
박건규 <geongyu09@gmail.com>
2025-06-26 14:45:08 +0900

    feat: 웹에서 보내는 라우팅 요청에 대하여 변환되어야 하는 쿼리값을 변환해주는 유틸 함수 정의
    #25

--- src/utils/bridge/index.ts
+++ src/utils/bridge/index.ts
@@ -1,3 +1,5 @@
+import PATH_TO_ROUTE from "@constants/bridge";
+import { Href } from "expo-router";
 import { RefObject } from "react";
 import WebView from "react-native-webview";
 
@@ -6,4 +8,27 @@ export const postMessage = (
   message: string,
 ) => {
   ref.current?.postMessage(message);
+};
+
+interface GetPathToRouteProps {
+  path: keyof typeof PATH_TO_ROUTE;
+  params?: { [key: string]: string | number }[];
+}
+
+export const getPathToRoute = ({ path, params }: GetPathToRouteProps): Href => {
+  let result = PATH_TO_ROUTE[path] as string;
+  if (!result) {
+    throw new Error(`Path not found for key: ${path}`);
+  }
+
+  // params 배열의 각 원소를 순회하며 [key]를 value로 치환
+  if (params && Array.isArray(params)) {
+    params.forEach((paramObj) => {
+      Object.entries(paramObj).forEach(([k, v]) => {
+        result = result.replace(new RegExp(`\\[${k}\\]`, "g"), String(v));
+      });
+    });
+  }
+
+  return result as Href;
 };
