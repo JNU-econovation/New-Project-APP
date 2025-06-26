9016768e0a97ea3c6056a0fd66f212d7ce5f5aea
박건규 <geongyu09@gmail.com>
2025-06-26 21:03:10 +0900

    feat: Text 공통 컴포넌트에 italic props 추가
    #27

--- src/components/common/shared/ui/Text/index.tsx
+++ src/components/common/shared/ui/Text/index.tsx
@@ -16,6 +16,7 @@ interface TextProps {
     | "thin";
   textAlign?: string;
   opacity?: number;
+  Italic?: boolean;
 }
 
 export default styled.Text<TextProps>`
@@ -24,4 +25,6 @@ export default styled.Text<TextProps>`
   font-family: ${({ fontWeight = "regular" }) => `pretendard-${fontWeight}`};
   text-align: ${({ textAlign = "left" }) => textAlign};
   opacity: ${({ opacity = 1 }) => opacity};
+  font-style: ${({ Italic }) => (Italic ? "italic" : "normal")};
+  ${({ Italic }) => Italic && "transform: skewX(-18deg);"}
 `;
