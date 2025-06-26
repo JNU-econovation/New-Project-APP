b9eed9a3d60c0c99e318175b21c5b4fa628ca67f
박건규 <geongyu09@gmail.com>
2025-06-26 14:43:29 +0900

    feat: 웹에서 보내는 라우팅 요청을 rotue-to로 획일화 함에 따라서 웹에서 보내는 path의 값에 대하여 앱에 있는 Href의 값과 매핑 될 수 있도록 전역 상수 추가
    #25

---
+++ src/constants/bridge/index.ts
@@ -0,0 +1,12 @@
+import { Href } from "expo-router";
+
+type PathToRoute = { [key: string]: Href };
+
+const PATH_TO_ROUTE: PathToRoute = {
+  "course-detail": "/(tabs)/map/course",
+  "start-travel": "/(tabs)/map/travel",
+  "mountain-course": "/(tabs)/map/course",
+  "manual-detail": "/(tabs)/home/safeManual/[manual]",
+} as const;
+
+export default PATH_TO_ROUTE;
