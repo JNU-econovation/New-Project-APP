67e4c04102dd6046e2580af7184a5e07fdf98dc6
박건규 <geongyu09@gmail.com>
2025-06-30 13:14:45 +0900

    feat: 코스 검색 스크린 구현
    #15

---
+++ src/app/(tabs)/home/course/search.tsx
@@ -0,0 +1,7 @@
+import CourseSearchWebview from "@screens/course/CourseSearchWebview";
+
+const CourseSearchScreen = () => {
+  return <CourseSearchWebview />;
+};
+
+export default CourseSearchScreen;
