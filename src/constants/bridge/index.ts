994c8127ad123d01ff6ae61ef89e955f248f77c3
박건규 <geongyu09@gmail.com>
2025-06-30 14:44:29 +0900

    feat: 모든 라우트 주소 반영
    #31

--- src/constants/bridge/index.ts
+++ src/constants/bridge/index.ts
@@ -5,9 +5,17 @@ type PathToRoute = { [key: string]: Href };
 const PATH_TO_ROUTE: PathToRoute = {
   "course-detail": "/(tabs)/map/course",
   "start-travel": "/travel",
-  "mountain-course": "/(tabs)/map/course",
   "manual-detail": "/(tabs)/home/safeManual/[manual]",
   "course-search": "/(tabs)/home/course/search",
+  "my-info": "/(tabs)/mypage/myInfo",
+  faq: "/(tabs)/mypage/faq",
+  "travel-log": "/(tabs)/mypage/travelLog",
+  "course-bookmark": "/(tabs)/mypage/courseBookmark",
+  "notification-setting": "/(tabs)/mypage/notificationSetting",
+  "change-password": "/(tabs)/mypage/changePassword",
+  "check-terms": "/(tabs)/mypage/checkTerms",
+  inquiry: "/(tabs)/mypage/inquiry",
+  "mountain-course": "/(tabs)/home/course/[mountainName]",
 } as const;
 
 export default PATH_TO_ROUTE;
