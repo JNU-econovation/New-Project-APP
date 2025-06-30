80cede9ec0609da09abb0a522d50f599d0e7c099
박건규 <geongyu09@gmail.com>
2025-06-30 19:59:50 +0900

    feat: 문의 스크린 구현
    #31

---
+++ src/app/(tabs)/mypage/inquiry.tsx
@@ -0,0 +1,12 @@
+import MypageInquiryWebview from "@screens/mypage/MypageInquiryWebview";
+import ScreenContainer from "@shared/layout/Screen";
+
+const InquiryScreen = () => {
+  return (
+    <ScreenContainer>
+      <MypageInquiryWebview />
+    </ScreenContainer>
+  );
+};
+
+export default InquiryScreen;
