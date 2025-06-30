5cc6e30ea77220e53c28a5a308211e429b159201
박건규 <geongyu09@gmail.com>
2025-06-30 19:59:17 +0900

    feat: faq 스크린 구현

---
+++ src/app/(tabs)/mypage/faq.tsx
@@ -0,0 +1,12 @@
+import MypageFaqWebview from "@screens/mypage/MypageFaqWebview";
+import ScreenContainer from "@shared/layout/Screen";
+
+const FaqScreen = () => {
+  return (
+    <ScreenContainer>
+      <MypageFaqWebview />
+    </ScreenContainer>
+  );
+};
+
+export default FaqScreen;
