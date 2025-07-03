b0b46275cd05c12e22d2829b0a97ff4f337a9e11
박건규 <geongyu09@gmail.com>
2025-07-03 19:57:58 +0900

    chore: 더이상 사용하지 않는 inquiry, faq 스크린 코드 제거

--- src/components/feature/screens/mypage/MypageInquiryWebview/index.tsx
+++
@@ -1,8 +0,0 @@
-import PATH_ROUTE from "@constants/pathRoute";
-import WebViewWithInjected from "@entities/WebViewWithInjected";
-
-const MypageInquiryWebview = () => {
-  return <WebViewWithInjected source={{ uri: PATH_ROUTE.WEBVIEW.MYPAGE }} />;
-};
-
-export default MypageInquiryWebview;
