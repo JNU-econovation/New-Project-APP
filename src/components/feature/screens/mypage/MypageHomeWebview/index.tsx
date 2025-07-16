0e7e16601a5a1aab83ed6279cb39b88f102e6dbd
박건규 <geongyu09@gmail.com>
2025-07-16 15:18:24 +0900

    feat: 마이 페이지 홈에서 onMessage 로직 추가
    #31

--- src/components/feature/screens/mypage/MypageHomeWebview/index.tsx
+++ src/components/feature/screens/mypage/MypageHomeWebview/index.tsx
@@ -1,8 +1,37 @@
 import PATH_ROUTE from "@constants/pathRoute";
 import WebViewWithInjected from "@entities/WebViewWithInjected";
+import useLogout from "@hooks/feature/useLogout";
 
 const MypageHomeWebview = () => {
-  return <WebViewWithInjected source={{ uri: PATH_ROUTE.WEBVIEW.MYPAGE }} />;
+  const { logout } = useLogout();
+
+  return (
+    <WebViewWithInjected
+      source={{ uri: PATH_ROUTE.WEBVIEW.MYPAGE }}
+      onMessage={({ method, name }) => {
+        if (name === "logout" && method === "DELETE") {
+          try {
+            logout();
+            return {
+              name: "logout",
+              status: "success",
+            };
+          } catch (error) {
+            console.error("Logout failed:", error);
+            return {
+              name: "logout",
+              status: "error",
+            };
+          }
+        }
+
+        return {
+          name,
+          status: "error",
+        };
+      }}
+    />
+  );
 };
 
 export default MypageHomeWebview;
