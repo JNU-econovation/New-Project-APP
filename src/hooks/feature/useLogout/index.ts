188683d60bc4836e9659dfa4fc214f60da94a28f
박건규 <geongyu09@gmail.com>
2025-07-19 01:11:11 +0900

    feat: 로그아웃시 전역 상태에서 토큰 제거
    #17

--- src/hooks/feature/useLogout/index.ts
+++ src/hooks/feature/useLogout/index.ts
@@ -1,9 +1,14 @@
+import { useTokenStore } from "@/src/store/secureStorage/useTokenStore";
 import { removeValueFromSecureStore } from "@utils/secureStore";
 
 const useLogout = () => {
+  const { clearTokens } = useTokenStore();
+
   const logout = async () => {
     try {
       await removeValueFromSecureStore("accessToken");
+      clearTokens();
+      console.log("[useLogout] 로그아웃 성공");
     } catch (error) {
       console.error("[useLogout] 로그아웃 중 에러 발생:", error);
       throw error;
