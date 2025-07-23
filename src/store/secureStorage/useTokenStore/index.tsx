62e220851bb6feafa741f0f2395df5990a5ee617
박건규 <geongyu09@gmail.com>
2025-07-24 01:02:10 +0900

    feat: useTokenStore에서만 secure store에 접근할 수 있도록 변경
    useTokenStore에서만 secure store에 접근가능하도록 설정하였습니다.
    사용하는 곳에서는 무조건 전역 변수를 사용하여 접근할 수 있습니다.
    이를 통해서 리액트에서 프로미스 로직을 사용하지 않아도 되며, 의존성을 가지는 모든 곳에서 데이터 변경에 대한 반응성을 가지게 됩니다.
    
    #36

--- src/store/secureStorage/useTokenStore/index.tsx
+++ src/store/secureStorage/useTokenStore/index.tsx
@@ -1,3 +1,4 @@
+import { setValueToSecureStore } from "@utils/secureStore";
 import { create } from "zustand";
 
 interface TokensStore {
@@ -6,19 +7,45 @@ interface TokensStore {
   accessTokenExpiredTime: number | null;
   setAccessToken: (token: string) => void;
   setRefreshToken: (token: string) => void;
-  // setAccessTokenExpiredTime: (time: string) => void;
   setAccessTokenExpiredTime: (time: number) => void;
   clearTokens: () => void;
 }
 
-export const useTokenStore = create<TokensStore>((set) => ({
+let prevAccessToken: string | null = null;
+let prevRefreshToken: string | null = null;
+let prevAccessTokenExpiredTime: number | null = null;
+
+/**
+ * 주의!! secure store에 접근하는 사이드 이팩트를 포함하고있습니다!!!
+ *
+ * 해당 store는 토큰을 관리하는 store입니다.
+ * secureStorage에 저장된 토큰을 관리하며, 토큰을 설정하거나 초기화하는 기능을 제공합니다.
+ */
+export const useTokenStore = create<TokensStore>((set, get) => ({
   accessToken: null,
   refreshToken: null,
   accessTokenExpiredTime: null,
-  setAccessToken: (token: string) => set({ accessToken: token }),
-  setRefreshToken: (token: string) => set({ refreshToken: token }),
-  setAccessTokenExpiredTime: (time: number) =>
-    set({ accessTokenExpiredTime: time }),
+  setAccessToken: (token: string) => {
+    prevAccessToken = get().accessToken;
+    set({ accessToken: token });
+    setValueToSecureStore("accessToken", token).catch(() =>
+      set({ accessToken: prevAccessToken }),
+    );
+  },
+  setRefreshToken: (token: string) => {
+    prevRefreshToken = get().refreshToken;
+    set({ refreshToken: token });
+    setValueToSecureStore("refreshToken", token).catch(() =>
+      set({ refreshToken: prevRefreshToken }),
+    );
+  },
+  setAccessTokenExpiredTime: (time: number) => {
+    prevAccessTokenExpiredTime = get().accessTokenExpiredTime;
+    set({ accessTokenExpiredTime: time });
+    setValueToSecureStore("accessTokenExpiredTime", `${time}`).catch(() =>
+      set({ accessTokenExpiredTime: prevAccessTokenExpiredTime }),
+    );
+  },
   clearTokens: () =>
     set({
       accessToken: null,
