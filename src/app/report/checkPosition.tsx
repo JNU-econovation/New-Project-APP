54e886710b29e6eadba9f4b3a6735ccc41be6d70
박건규 <geongyu09@gmail.com>
2025-07-29 15:27:04 +0900

    feat: 상세 신고에서 신고 위치 설정 스크린 구현
    #36

--- src/app/report/checkPosition.tsx
+++ src/app/report/checkPosition.tsx
@@ -1,12 +1,48 @@
+import WebViewWithInjected from "@components/common/entities/WebViewWithInjected";
+import PATH_ROUTE from "@constants/pathRoute";
+import useGetCurrentPosition from "@hooks/feature/useGetCurrentPosition";
 import ScreenContainer from "@shared/layout/Screen";
 import Header from "@shared/ui/Header";
-import Text from "@shared/ui/Text";
+import { useReportPositionStore } from "@store/report/useReportPositionStore";
 
 const CheckPositionScreen = () => {
+  const { isLoading, location } = useGetCurrentPosition();
+  const { setReportPosition } = useReportPositionStore();
+
+  if (isLoading || !location) {
+    return null;
+  }
+
+  const {
+    coords: { latitude, longitude },
+  } = location;
+
   return (
     <ScreenContainer>
       <Header />
-      <Text>지도 웹뷰 보여질 예정</Text>
+      {/* TODO: 분리 */}
+      <WebViewWithInjected
+        source={{ uri: PATH_ROUTE.WEBVIEW.CHECK_POSITION(latitude, longitude) }}
+        onMessage={({ method, name, body }) => {
+          if (name === "set-report-position" && method === "POST") {
+            const { latitude, longitude } = body as {
+              latitude: number;
+              longitude: number;
+            };
+            setReportPosition({ latitude, longitude });
+
+            return {
+              name: "set-report-position",
+              status: "success",
+            };
+          }
+          return {
+            name: "set-report-position",
+            status: "error",
+            message: "Invalid request",
+          };
+        }}
+      />
     </ScreenContainer>
   );
 };
