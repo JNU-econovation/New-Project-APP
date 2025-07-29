651551ac4adcb9649a0422664e9d1f5fc8212c1a
박건규 <geongyu09@gmail.com>
2025-07-29 15:29:29 +0900

    feat: LoginWebView에서 put token 브리지 onMessage 구현
    #36

--- src/components/feature/widget/LoginWebView/index.tsx
+++ src/components/feature/widget/LoginWebView/index.tsx
@@ -1,26 +1,35 @@
 import PATH_ROUTE from "@constants/pathRoute";
 import WebViewWithInjected from "@entities/WebViewWithInjected";
-import { setValueToSecureStore } from "@utils/secureStore";
+import { useTokenStore } from "@store/secureStorage/useTokenStore";
+import { router } from "expo-router";
 import { useRef } from "react";
 import WebView from "react-native-webview";
 
-interface KakaoTokens {
-  accessToken: string;
-  requestToken: string;
-}
-
 const LoginWebView = () => {
   const ref = useRef<WebView>(null);
+
+  const { setAccessToken, setRefreshToken, setAccessTokenExpiredTime } =
+    useTokenStore();
 
   return (
     <WebViewWithInjected
       ref={ref}
       source={{ uri: PATH_ROUTE.WEBVIEW.KAKAO_LOGIN }}
       onMessage={({ method, name, body }) => {
-        if (name === "request-kakao" && method === "PUT") {
-          const { accessToken, requestToken } = body as KakaoTokens;
-          setValueToSecureStore("accessToken", accessToken);
-          setValueToSecureStore("requestToken", requestToken);
+        if (name === "put-token" && method === "POST") {
+          const { accessToken, refreshToken, accessTokenExpiredTime } =
+            body as {
+              accessToken: string;
+              refreshToken: string;
+              accessTokenExpiredTime: number;
+            };
+
+          setAccessToken(accessToken);
+          setRefreshToken(refreshToken);
+          setAccessTokenExpiredTime(accessTokenExpiredTime);
+
+          router.dismissAll();
+          router.replace("/(tabs)/home");
         }
       }}
       loadingBar
