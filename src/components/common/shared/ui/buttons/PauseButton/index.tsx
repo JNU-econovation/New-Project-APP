3706c16a81f35ec7247c97ac45aca77e37aee561
박건규 <geongyu09@gmail.com>
2025-08-09 00:00:43 +0900

    feat: PauseButton 공통 컴포넌트 구현
    #27

---
+++ src/components/common/shared/ui/buttons/PauseButton/index.tsx
@@ -0,0 +1,71 @@
+import styled from "@emotion/native";
+import { COLORS } from "@styles/colorPalette";
+import Animated, {
+  Easing,
+  ReduceMotion,
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { TouchableOpacityProps } from "react-native";
+
+interface PauseButtonProps extends TouchableOpacityProps {}
+
+const PauseButton = (props: PauseButtonProps) => {
+  const scale = useSharedValue(1);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
+
+  const onPressIn = () => {
+    scale.value = withTiming(0.98, {
+      duration: 150,
+      easing: Easing.inOut(Easing.ease),
+      reduceMotion: ReduceMotion.System,
+    });
+  };
+
+  const onPressOut = () => {
+    scale.value = withTiming(1, {
+      duration: 150,
+      easing: Easing.inOut(Easing.ease),
+      reduceMotion: ReduceMotion.System,
+    });
+  };
+
+  return (
+    <Animated.View style={animatedStyle}>
+      <ActionButton
+        activeOpacity={0.8}
+        onPressIn={onPressIn}
+        onPressOut={onPressOut}
+        {...props}
+      >
+        <PauseBar />
+        <PauseBar />
+      </ActionButton>
+    </Animated.View>
+  );
+};
+
+const ActionButton = styled.TouchableOpacity`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  background-color: ${COLORS.mainGreen};
+  width: 64px;
+  height: 64px;
+  border-radius: 32px;
+`;
+
+const PauseBar = styled.View`
+  height: 32px;
+  width: 6px;
+  background-color: ${COLORS.mainWhite};
+  border-radius: 1px;
+`;
+
+export default PauseButton;
