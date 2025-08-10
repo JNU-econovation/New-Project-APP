10eb4624eab777cad0b1f670313c694ae8a37458
박건규 <geongyu09@gmail.com>
2025-08-11 03:08:08 +0900

    feat: TravelMonitorSection 기능 구현
    #27

--- src/components/feature/screens/travel/TravelWithCourseMapWebview/index.tsx
+++ src/components/feature/screens/travel/TravelWithCourseMapWebview/index.tsx
@@ -1,13 +1,11 @@
 import PATH_ROUTE from "@constants/pathRoute";
 import WebViewWithInjected from "@entities/WebViewWithInjected";
 import useTravelCourse from "@hooks/feature/travel/useTravelCourse";
-// import useTravelWithCourse from "@hooks/feature/travel/useTravelWithCourse";
 import { useLocalSearchParams } from "expo-router";
 import { useEffect } from "react";
 
 const TravelWithCourseMapWebview = () => {
   const { courseId } = useLocalSearchParams<{ courseId: string }>();
-  // const { ref, startTravel } = useTravelWithCourse({
   const { ref, connect } = useTravelCourse({
     courseId: courseId ?? "1",
   });
