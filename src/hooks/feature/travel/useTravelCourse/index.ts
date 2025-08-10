2e415e1e3524e40b9035995a08b6f103d060af2a
박건규 <geongyu09@gmail.com>
2025-08-11 01:45:18 +0900

    feat: 코스를 지정한 산행을 하는 로직을 가진 useTravelCourse훅 구현
    #27

---
+++ src/hooks/feature/travel/useTravelCourse/index.ts
@@ -0,0 +1,197 @@
+import * as Location from "expo-location";
+import useSetMapPolylineBridge from "@hooks/feature/bridge/useSetMapPolylineBridge";
+import useGetCoursePathByCourseId from "@hooks/feature/useGetCoursePathByCourseId";
+import SocketManager from "@service/socket/manager";
+import useToast from "@service/toast";
+import { useTokenStore } from "@store/secureStorage/useTokenStore";
+import useTravelStateStore from "@store/travel";
+import { COLORS } from "@styles/colorPalette";
+import { router } from "expo-router";
+import { useEffect, useState } from "react";
+
+const TRAVEL_SOCKET_URL =
+  process.env.EXPO_PUBLIC_TRAVEL_NAVIGATE_SOCKET_URL || "";
+// process.env.EXPO_PUBLIC_TRAVEL_SOCKET_URL || "";
+const TRAVEL_SOCKET_INTERVAL = 1200;
+
+interface UseTravelCourseProps {
+  courseId: string;
+}
+const useTravelCourse = ({ courseId }: UseTravelCourseProps) => {
+  const socketManager = SocketManager.getInstance();
+  const { accessToken } = useTokenStore.getState();
+  const { setTravelState, travelState, intervalId, setIntervalId } =
+    useTravelStateStore();
+  const showToast = useToast();
+  const coordinates = useGetCoursePathByCourseId({ courseId });
+  const { ref, sendSetMapPolylineMessage } = useSetMapPolylineBridge();
+  const [shouldStartTravel, setShouldStartTravel] = useState(false);
+
+  // location이 준비되면 start 메시지 전송
+  useEffect(() => {
+    (async () => {
+      const socket = socketManager.getSocket(TRAVEL_SOCKET_URL);
+      let { latitude, longitude } = (await Location.getCurrentPositionAsync({}))
+        .coords;
+      if (socket && shouldStartTravel) {
+        socket.sendMessage({
+          event: "start",
+          data: {
+            courseId,
+            coordinate: [longitude, latitude],
+            time: Date.now(),
+          },
+        });
+        // console.log("[useTravelCourse] start 메시지 전송됨");
+        setShouldStartTravel(false);
+      }
+    })();
+  }, [shouldStartTravel, courseId]);
+
+  const connect = () => {
+    if (!accessToken) {
+      console.warn("[useTravelCourse] 토큰이 정의되지 않았습니다.");
+      return;
+    }
+
+    socketManager.makeNewConnection({
+      url: TRAVEL_SOCKET_URL,
+      token: accessToken,
+      onOpen: () => {
+        console.log("[useTravelCourse] 소켓 연결 성공");
+        // callback?.();
+        setShouldStartTravel(true);
+      },
+      onClose: () => {
+        console.warn("[useTravelCourse] 소켓 연결이 종료되었습니다.");
+        setTravelState("idle");
+      },
+      onError: (error) => {
+        console.error("[useTravelCourse] 소켓 연결 오류:", error);
+        setTravelState("idle");
+      },
+      onMessage: ({ event, status, data }) => {
+        console.log("소캣 메시지 수신:", { event, status, data });
+        if (event === "current-position" && status === "success" && data) {
+          console.log("[useTravelCourse] 현재 위치:", data);
+
+          if (
+            typeof data.index === "number" &&
+            typeof data.isArrived === "boolean" &&
+            typeof data.isDeviation === "boolean"
+          ) {
+            const { index, isArrived, isDeviation, travelDistance } = data;
+            console.log("[useTravelWithCourse] 서버로부터 받은 데이터:", data);
+
+            sendSetMapPolylineMessage([
+              {
+                path: coordinates.slice(0, index + 1),
+                strokeColor: COLORS.gray20,
+              },
+              {
+                path: coordinates.slice(index),
+                strokeColor: COLORS.mainGreen,
+              },
+            ]);
+
+            if (isArrived) {
+              setTravelState("completed");
+              showToast({
+                type: "success",
+                text1: "여행이 완료되었습니다.",
+                text2: "즐거운 여행 되세요!",
+              });
+
+              if (intervalId) {
+                clearInterval(intervalId);
+                setIntervalId(null);
+              }
+
+              // 종료
+              Location.getCurrentPositionAsync({})
+                .then(({ coords: { longitude, latitude } }) => {
+                  socketManager.getSocket(TRAVEL_SOCKET_URL)?.sendMessage({
+                    event: "stop",
+                    data: {
+                      coordinate: [longitude, latitude],
+                      courseId,
+                      time: Date.now(),
+                    },
+                  });
+                })
+                .then(() => {
+                  setTimeout(() => {
+                    socketManager.disconnectSocket(TRAVEL_SOCKET_URL);
+                  }, 1500);
+
+                  setTimeout(() => {
+                    router.back();
+                  }, 3000);
+                });
+            }
+
+            if (isDeviation) {
+              showToast({
+                type: "error",
+                text1: "경로 이탈 감지",
+                text2: "정해진 경로를 벗어났습니다.",
+              });
+            }
+          }
+        }
+        if (event === "start" && status === "success" && data) {
+          console.log("[useTravelCourse] 여행 시작:", data);
+          setTravelState("in-progress");
+          const newIntervalId = setInterval(async () => {
+            if (!TRAVEL_SOCKET_URL) {
+              console.warn(
+                "[useTravelWithCourse] 소켓 URL이 정의되지 않았습니다.",
+              );
+              return;
+            }
+            let socket = socketManager.getSocket(TRAVEL_SOCKET_URL);
+            if (socket) {
+              let { latitude, longitude } = (
+                await Location.getCurrentPositionAsync({})
+              ).coords;
+              // const { latitude, longitude } = location?.coords;
+              socket.sendMessage({
+                event: "current-position",
+                data: {
+                  coordinate: [longitude, latitude],
+                  courseId,
+                },
+              });
+            }
+          }, TRAVEL_SOCKET_INTERVAL);
+          setIntervalId(newIntervalId);
+        }
+        if (event === "pause" && status === "success" && data) {
+          console.log("[useTravelCourse] 여행 일시 정지:", data);
+        }
+        if (event === "restart" && status === "success" && data) {
+          console.log("[useTravelCourse] 여행 재시작:", data);
+        }
+        if (event === "stop" && status === "success" && data) {
+          console.log("[useTravelCourse] 여행 끝:", data);
+        }
+      },
+    });
+  };
+
+  const disconnect = () => {
+    if (!TRAVEL_SOCKET_URL) {
+      console.warn("[useTravelCourse] 소켓 URL이 정의되지 않았습니다.");
+      return;
+    }
+    socketManager.disconnectSocket(TRAVEL_SOCKET_URL);
+  };
+
+  return {
+    ref,
+    connect,
+    disconnect,
+  };
+};
+
+export default useTravelCourse;
