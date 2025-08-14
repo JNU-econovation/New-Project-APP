aa52b6b4e560d083698b71d400f3bd0a78b6eb02
박건규 <geongyu09@gmail.com>
2025-08-14 22:50:54 +0900

    feat: NoticeBarProvider를 global _layout에 적용
    #2

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,5 @@
 import ModalProvider from "@service/modal/provider";
+import { NoticeBarProvider } from "@service/notice-bar";
 import QueryProvider from "@service/query/provider";
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
@@ -7,46 +8,51 @@ import Toast from "react-native-toast-message";
 export default function RootLayout() {
   return (
     <QueryProvider>
-      <ModalProvider>
-        <StatusBar barStyle={"dark-content"} />
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen
-            name="(tabs)"
-            options={{
-              animation: "fade",
+      <NoticeBarProvider>
+        <ModalProvider>
+          <StatusBar barStyle={"dark-content"} />
+          <Stack
+            screenOptions={{
+              headerShown: false,
+              contentStyle: {
+                backgroundColor: "white",
+              },
             }}
-          />
-          <Stack.Screen name="index" />
-          <Stack.Screen name="login" />
-          <Stack.Screen
-            name="loginModal"
-            options={{
-              presentation: "modal",
-              headerShown: true,
-              headerTitle: "카카오 로그인",
-            }}
-          />
-          <Stack.Screen
-            name="report"
-            options={{
-              animation: "fade",
-              animationDuration: 100,
-            }}
-          />
-          <Stack.Screen
-            name="travel"
-            options={{
-              animation: "fade",
-              animationDuration: 100,
-            }}
-          />
-        </Stack>
-      </ModalProvider>
-      <Toast />
+          >
+            <Stack.Screen
+              name="(tabs)"
+              options={{
+                animation: "fade",
+              }}
+            />
+            <Stack.Screen name="index" />
+            <Stack.Screen name="login" />
+            <Stack.Screen
+              name="loginModal"
+              options={{
+                presentation: "modal",
+                headerShown: true,
+                headerTitle: "카카오 로그인",
+              }}
+            />
+            <Stack.Screen
+              name="report"
+              options={{
+                animation: "fade",
+                animationDuration: 100,
+              }}
+            />
+            <Stack.Screen
+              name="travel"
+              options={{
+                animation: "fade",
+                animationDuration: 100,
+              }}
+            />
+          </Stack>
+        </ModalProvider>
+        <Toast />
+      </NoticeBarProvider>
     </QueryProvider>
   );
 }
