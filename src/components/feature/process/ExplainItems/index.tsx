b7e3215b6e2c90b0e3561449e956818698eaa0dd
박건규 <geongyu09@gmail.com>
2025-08-15 00:02:03 +0900

    chore: import문으로 assets를 가져오던 부분에서 빌드가 되지 않는 부분 수정
    #2

--- src/components/feature/process/ExplainItems/index.tsx
+++ src/components/feature/process/ExplainItems/index.tsx
@@ -4,7 +4,6 @@ import Guide2Image from "@shared/ui/Images/Guide2";
 import Guide3Image from "@shared/ui/Images/Guide3";
 import Text from "@shared/ui/Text";
 import { View, Image } from "react-native";
-import Guide3Png from "@assets/images/guide3.png";
 
 const SCREEN_HEIGHT = 430;
 
@@ -106,8 +105,9 @@ const ExplainItem4 = () => {
         <Text fontSize={30} fontWeight="bold" color="mainGreen">
           깃대종 식물 키우기
         </Text>
+        {/* <Guide3Image /> */}
         <Image
-          source={require(Guide3Png)}
+          source={require("@/assets/images/guide3.png")}
           style={{ width: 197, height: 197 }}
         />
         <View>
