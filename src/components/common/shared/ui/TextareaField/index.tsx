97445f35d2b65368e7f6123b1ca4da4eda88caf9
박건규 <geongyu09@gmail.com>
2025-08-14 19:35:16 +0900

    feat: TextAreaField에서 helper 를 사용할 수 있도록 추가
    #2

--- src/components/common/shared/ui/TextareaField/index.tsx
+++ src/components/common/shared/ui/TextareaField/index.tsx
@@ -1,22 +1,48 @@
 import FieldLayout from "@shared/layout/FieldLayout";
 import Spacing from "@shared/layout/Spacing";
+import Text from "@shared/ui/Text";
 import Textarea from "@shared/ui/Textarea";
 
 interface TextAreaFieldProps extends React.ComponentProps<typeof Textarea> {
   title: string;
+  titleSpacing?: number;
+  helperText?: string;
+  helperTextSpacing?: number;
+  helperTextProps?: React.ComponentProps<typeof Text>;
+  titleSideComponent?: React.ReactNode;
+  contentSideComponent?: React.ReactNode;
 }
 
-const TextAreaField = ({ title, ...props }: TextAreaFieldProps) => {
+/**
+ * 서비스에서 사용되는 모든 TextAreaField의 공통 UI 를 정의합니다.
+ * 사용시 도메인에 맞는 로직이 들어간 새로운 컴포넌트로 만들어서 사용하면 됩니다.
+ */
+const TextAreaField = ({
+  title,
+  titleSpacing = 14,
+  helperText,
+  helperTextSpacing = 4,
+  helperTextProps,
+  titleSideComponent,
+  contentSideComponent,
+  ...props
+}: TextAreaFieldProps) => {
   return (
-    <FieldLayout
-      title={title}
-      content={
-        <>
-          <Spacing size={14} />
-          <Textarea {...props} />
-        </>
-      }
-    />
+    <>
+      <FieldLayout
+        title={title}
+        titleSideComponent={titleSideComponent}
+        content={
+          <>
+            <Spacing size={titleSpacing} />
+            <Textarea {...props} />
+          </>
+        }
+        contentSideComponent={contentSideComponent}
+      />
+      <Spacing size={helperTextSpacing} />
+      <Text {...helperTextProps}>{helperText}</Text>
+    </>
   );
 };
 
