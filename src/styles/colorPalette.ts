50fac497f9a8a1437524b8871b14ced021d41400
박건규 <geongyu09@gmail.com>
2025-08-15 17:49:02 +0900

    chore: disabledGray 색 추가
    #45

--- src/styles/colorPalette.ts
+++ src/styles/colorPalette.ts
@@ -3,11 +3,14 @@ export const COLORS = {
   green10: "#d7e5de",
   green20: "#eaf1ee",
   mainWhite: "#ffffff",
+
   subGray: "#dadada",
-  gray200: "#828282",
+  gray200: "#828282", //disabled text
   gray20: "#7f7f7f",
   gray30: "#d9d9d9",
   inputGray: "#f7f7f7",
+  disabledGray: "#eaeaea",
+
   black: "#000000",
   kakaoYellow: "#fae64d",
   mainRed: "#FF5151",
