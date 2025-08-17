b4a31b5282d350979cc981454b97126bd08c5659
박건규 <geongyu09@gmail.com>
2025-08-17 22:36:12 +0900

    refacor: OnboardFormSection 에서 timestampToMinutesSeconds를 공통 utils함수로 사용하도록 수정
    #47

--- src/components/feature/screens/onboarding/OnboardFormSection/index.tsx
+++ src/components/feature/screens/onboarding/OnboardFormSection/index.tsx
@@ -5,16 +5,10 @@ import DefaultButton from "@shared/ui/buttons/DefaultButton";
 import Text from "@shared/ui/Text";
 import TextAreaField from "@shared/ui/TextareaField";
 import { COLORS } from "@styles/colorPalette";
+import { timestampToMinutesSeconds } from "@utils/time";
 import { router } from "expo-router";
 import { useEffect, useRef, useState } from "react";
 import * as z from "zod";
-
-const timestampToMinutesSeconds = (ms: number) => {
-  const totalSeconds = Math.max(0, Math.floor(ms / 1000));
-  const minutes = String(Math.floor(totalSeconds / 60)).padStart(2, "0");
-  const seconds = String(totalSeconds % 60).padStart(2, "0");
-  return `${minutes}:${seconds}`;
-};
 
 const OnboardFormSection = () => {
   const verificationRef = useRef({ deadline: 0 });
