36f09c40ce29c9437e969a59a50d1423fb1d46b9
박건규 <geongyu09@gmail.com>
2025-08-17 22:34:23 +0900

    feat: useProfileSetForm 구현
    #47

---
+++ src/hooks/feature/form/useProfileSetForm/index.ts
@@ -0,0 +1,37 @@
+import { useForm, useFormContext } from "react-hook-form";
+
+interface ProfileFormValues {
+  nickname: string;
+  phoneNumber: string;
+  verificationCode: string;
+  email: string;
+
+  // meta
+  isNicknameValid: boolean;
+  phoneNumberVerificationCount: number;
+  isPhoneNumberValid: boolean;
+  verificationDeadline: number;
+}
+
+const useProfileSetForm = () => {
+  return useForm<ProfileFormValues>({
+    defaultValues: {
+      // form field
+      nickname: "",
+      phoneNumber: "010-",
+      verificationCode: "",
+      email: "",
+
+      // meta
+      isNicknameValid: false,
+      isPhoneNumberValid: false,
+      phoneNumberVerificationCount: 0,
+    },
+  });
+};
+
+export const useProfileSetFormContext = () => {
+  return useFormContext<ProfileFormValues>();
+};
+
+export default useProfileSetForm;
