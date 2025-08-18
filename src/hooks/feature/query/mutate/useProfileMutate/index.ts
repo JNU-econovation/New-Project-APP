5a1b3ebe554b66ada4f6265450820ffc29225201
박건규 <geongyu09@gmail.com>
2025-08-18 16:54:32 +0900

    feat: SubmitButton 을 통하여 올바르게 요청이 되도록 로직 추가
    #47

--- src/hooks/feature/query/mutate/useProfileMutate/index.ts
+++ src/hooks/feature/query/mutate/useProfileMutate/index.ts
@@ -2,12 +2,10 @@ import { postProfile, USER_PROFILE_API_PATH } from "@api/v1/users/profile";
 import { useMutation } from "@tanstack/react-query";
 
 const useProfileMutate = () => {
-  const mutation = useMutation({
+  return useMutation({
     mutationKey: [USER_PROFILE_API_PATH],
     mutationFn: postProfile,
   });
-
-  return mutation;
 };
 
 export default useProfileMutate;
