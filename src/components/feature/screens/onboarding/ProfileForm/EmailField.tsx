677efdb522226d32a7969fd905a21ceb90baf06b
박건규 <geongyu09@gmail.com>
2025-08-18 15:34:25 +0900

    feat: EmailField 전역으로 helper state 적용
    #47

--- src/components/feature/screens/onboarding/ProfileForm/EmailField.tsx
+++ src/components/feature/screens/onboarding/ProfileForm/EmailField.tsx
@@ -1,39 +1,30 @@
 import { COLORS } from "@styles/colorPalette";
 
 import TextAreaField from "@components/common/shared/ui/TextareaField";
+import HELPER from "@constants/inputField/helper";
 import { useProfileSetFormContext } from "@hooks/feature/form/useProfileSetForm";
-import { useState } from "react";
 import { Controller } from "react-hook-form";
 import { z } from "zod";
 
 const emailSchema = z.string().email("올바른 이메일 주소를 입력해 주세요.");
 
-const helperText = {
-  duplicated: "이미 가입된 이메일입니다.",
-  invalid: "올바른 이메일 주소를 입력해 주세요.",
-  none: undefined,
-  fit: undefined,
-};
-
 const EmailField = () => {
-  const { control, getValues, setValue } = useProfileSetFormContext();
-  const [helperState, setHelperState] =
-    useState<keyof typeof helperText>("none");
+  const { control, setValue, watch } = useProfileSetFormContext();
 
   const checkEmail = (email: string) => {
     const result = emailSchema.safeParse(email);
 
     if (email.length === 0) {
-      setHelperState("none");
+      setValue("emailFieldHelperState", "NONE");
       return;
     }
 
     if (result.success) {
-      setHelperState("fit");
+      setValue("emailFieldHelperState", "FIT");
       return;
     }
 
-    setHelperState("invalid");
+    setValue("emailFieldHelperState", "INVALID");
   };
 
   return (
@@ -49,7 +40,7 @@ const EmailField = () => {
           borderColor="inputGray"
           paddingVertical={16}
           placeholderTextColor={COLORS.subGray}
-          helperText={helperText[helperState]}
+          helperText={HELPER.PROFILE_FORM.EMAIL[watch("emailFieldHelperState")]}
           onChangeText={(email) => {
             checkEmail(email);
             onChange(email);
